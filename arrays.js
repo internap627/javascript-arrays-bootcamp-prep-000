94c990cdb59754d9ff86510cff03857f118cffd1
Nkosi Phillip <internap627@gmail.com>
2018-09-05 02:34:24 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,3 +3,8 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 function addElementToBeginningOfArray(arr, element){
   return arr.push(element);
 }
+
+function destructivelyAddElementToBeginningOfArray(arr, element){
+  arr.push(element);
+  return arr;
+}
